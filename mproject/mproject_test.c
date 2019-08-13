40288f75adfff7ccedbf2a543f30ff789a42f177
fanpengfei1 <fanpengfei@jd.com>
2019-08-13 10:01:08 +0000

    update for code review

--- mproject/mproject_test.c
+++ mproject/mproject_test.c
@@ -5,43 +5,18 @@
 #include <string.h>
 #include <sys/mman.h>
 
+#include "mp_malloc.h"
+
 /* TODO:
- * add sig process, when sigment print address contect 
+ * add sig process, when sigment print address content 
  * */
-
-void * malloc_align_page(size_t memsize)
-{
-    void *start = NULL;
-
-    if (memsize == 0) {
-        return NULL;
-    }
-
-    long pagesize = sysconf(_SC_PAGE_SIZE);
-    if (pagesize == -1) {
-        perror("sysconf err");
-        return NULL;
-    }
-
-    size_t datasize = memsize + pagesize * 2;
-    void *origin_start = malloc(datasize);
-    if(origin_start == NULL) {
-        perror("malloc");
-        return NULL;
-    }
-    memset(origin_start, 0, datasize);
-
-    long mask = pagesize - 1;
-    start = (void *)((long)(origin_start + pagesize) & ~mask);
-
-    return start;
-}
 
 int main()
 {
     /* malloc */
     size_t plen = 4097;
-    char *p = (char *)malloc_align_page(plen);
+
+    char *p = (char *)MP_MALLOC(plen);
     if (p == NULL) {
         return -1;
     }
@@ -51,8 +26,8 @@ int main()
     p[4] = '1';
 
     /* lock */
-    if (mprotect(p, (size_t)(plen), PROT_READ) == -1) {
-        perror("Error: mportect %s:%d", __func__, __LINE__);
+    if (MP_RLOCK(p, plen) == -1) {
+        perror("Error: mportect");
         return -1;
     }
 
